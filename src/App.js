33436ad6d7019c4f40c7b149db958ecacea9795c
Jamal Ahmad <qja786@gmail.com>
2023-04-19 15:44:24 +0530

    Day1 evening commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-
 function App() {
   return (
     <>
-    <div class="navbar-collapse">
-      <nav className="mennavbar-nav mr-auto">
-      <ul>
-        <li className='nav-item active'> <a class="nav-link" href="#">Home</a></li>
-        <li className='nav-item active'> <a class="nav-link" href="#">About</a></li>
-        <li className='nav-item active'> <a class="nav-link" href="#">Contact</a></li>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark" data-bs-theme="dark">
+    <div className="container-fluid">
+    <a class="navbar-brand" href="/">TextUtil</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Contact</a>
+        </li>
       </ul>
-    </nav>
-   </div>
-   <div className="container">
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+  </nav>
+   <div classNameName="container">
     <p>
     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
     </p>
